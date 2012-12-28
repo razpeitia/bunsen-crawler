946a45ed087f0daed8c6c69cef1c6190bd66e23c
razpeitia <razpeitia@gmail.com>
2012-12-28 17:53:11 -0600

    200 to 20 fixed

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ url = "http://www.heroeslocales.com/bunsen/comics/"
 parser = etree.HTMLParser()
 tree = etree.parse(url, parser)
 
-links = [url + i for i in tree.xpath('//a[starts-with(@href, "200")]/@href')]
+links = [url + i for i in tree.xpath('//a[starts-with(@href, "20")]/@href')]
 
 with open("bunsen.txt", "wb") as f:
     for link in links:
